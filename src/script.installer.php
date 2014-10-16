578d4648c1d474b677834bcb75273a2c1e38d1e7
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-10-16 01:49:10 -0300

    Add the installer script

---
+++ src/script.installer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package   OSVimeo
+ * @contact   www.alledia.com, support@alledia.com
+ * @copyright 2014 Alledia.com, All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+require_once 'library/installer/include.php';
+
+
+defined('_JEXEC') or die();
+
+class PlgContentOSVimeoInstallerScript extends AllediaInstallerAbstract
+{
+}
