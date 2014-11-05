fbdfcd20c30e20be519782ca1737c6b32cb8644e
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-11-05 16:52:26 -0200

    Adapt to the new standalone installer
    No framework dependency while installing

--- src/script.installer.php
+++ src/script.installer.php
@@ -1,16 +1,20 @@
 <?php
 /**
  * @package   OSVimeo
- * @contact   www.alledia.com, support@alledia.com
+ * @contact   www.alledia.com, hello@alledia.com
  * @copyright 2014 Alledia.com, All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
-require_once 'library/installer/include.php';
-
-
 defined('_JEXEC') or die();
 
-class PlgContentOSVimeoInstallerScript extends AllediaInstallerAbstract
+require_once 'library/Installer/include.php';
+
+use Alledia\Installer\AbstractScript;
+
+/**
+ * Custom installer script
+ */
+class PlgContentOSVimeoInstallerScript extends AbstractScript
 {
 }
