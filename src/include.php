9f86cc3ed33c7d1084b32034f808b715dd1d5eab
Anderson Grudtner Martins <anderson@ostraining.com>
2016-02-17 15:49:29 -0200

    Update copyright year

--- src/include.php
+++ src/include.php
@@ -2,7 +2,7 @@
 /**
  * @package   OSVimeo
  * @contact   www.alledia.com, hello@alledia.com
- * @copyright 2014 Alledia, All rights reserved
+ * @copyright 2016 Alledia, All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
